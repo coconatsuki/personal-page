1d27d6a1491c508d24d50497cb3169bf160d923b
Amandine Latour <amandine.latour+github@gmail.com>
2019-03-20 18:46:08 +0900

    replace latour by pasin in the entire app

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
-class ContactMailer < ApplicationMailer
+# frozen_string_literal: true
 
+class ContactMailer < ApplicationMailer
   def contact_mail(firstname, lastname, email, message)
     @firstname = firstname
     @lastname = lastname
     @email = email
     @message = message
-    mail to: ENV['personal_mail_address'], subject: "Contact from #{firstname} #{lastname}, Personal page", from: 'contact@amandinelatour.fr'
+    mail to: ENV['personal_mail_address'], subject: "Contact from #{firstname} #{lastname}, Personal page", from: 'contact@amandine.pasin.fr'
   end
-
 end
