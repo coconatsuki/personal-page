b4467668f27bb820b64219e726556e80c5297c05
Amandine Latour <amandine.latour+github@gmail.com>
2019-04-03 17:09:41 +0900

    add an arrow to home page

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,19 +1,22 @@
-$(document).on('turbolinks:load', function() {
-  $('.special.cards .image.dimmable').dimmer({
-    on: 'hover'
+$(document).on("turbolinks:load", function() {
+  $(".special.cards .image.dimmable").dimmer({
+    on: "hover"
   });
 
-  $('.modal-button').click(function() {
-    $('#' + $(this).data('modal')).modal('show');
+  $(".modal-button").click(function() {
+    $("#" + $(this).data("modal")).modal("show");
   });
 
-  $('.ui.accordion')
-    .accordion({exclusive: false});
+  $(".ui.accordion").accordion({ exclusive: false });
 
-    $('.ui.dropdown')
-      .dropdown()
-    ;
+  $(".ui.dropdown").dropdown();
 
+  // Bouncing arrow bottom of full screen
+  // Different arrow choices from Font Awesome
+  // Fades away as you scroll down
 
-
+  $(window).scroll(function() {
+    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250);
+    //250 is fade pixels
+  });
 });
